c5dc990a61051b8e1b089a7b2ef280ee57d71d73
Qwreey <qwreey75@gmail.com>
2022-10-24 02:06:34 +0900

    Added ignore file and build script and readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+
+run ./build.sh and check dist/output.zip  
+and unzip that file into ~/.local/share/gnome-shell/extensions/app-volume-mixer@qwreey  
+for install
--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 glib-compile-schemas schemas
+
+rm -rf dist 2> /dev/null
+mkdir dist 2> /dev/null
+mkdir dist/build 2> /dev/null
+cp extension.js dist/build/
+cp prefs.js dist/build/
+cp metadata.json dist/build/
+cp volumeMixerPrefsPage.js dist/build/
+cp volumeMixerPopupMenu.js dist/build/
+cp volumeMixerAddFilterDialog.js dist/build/
+cp applicationStreamSlider.js dist/build/
+mkdir dist/build/schemas
+cp schemas/app-volume-mixer.gschema.xml dist/build/schemas/
+cp schemas/gschemas.compiled dist/build/schemas/
+zip -r dist/output.zip dist/build
+
