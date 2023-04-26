cfadc2150a7e6afec0ac6617ad3966a274f1fdc7
Qwreey <qwreey75@gmail.com>
2023-04-26 19:44:23 +0900

    Update dndQuickToggleHandler.js: Add translate support

--- libs/dndQuickToggleHandler.js
+++ libs/dndQuickToggleHandler.js
@@ -6,31 +6,32 @@ const DndQuickToggle = GObject.registerClass(
   class DndQuickToggle extends QuickToggle {
     _init() {
       super._init({
-        title: "Do Not Disturb",
+        title: _('Do Not Disturb'),
         iconName: "notifications-disabled-symbolic",
       });
 
-      this.label = "Do Not Disturb";
-
+      // this.label = "Do Not Disturb";
       this._settings = new Gio.Settings({
         schema_id: "org.gnome.desktop.notifications",
       });
 
-      this._changedId = this._settings.connect("changed::show-banners", () =>
-        this._sync()
-      );
+      this._changedId = this._settings.connect("changed::show-banners", this._sync.bind(this));
 
       this.connectObject(
-        "destroy",
-        () => this._settings.run_dispose(),
-        "clicked",
-        () => this._toggleMode(),
+        // Destroy event
+        "destroy", this._settings.run_dispose.bind(this._settings),
+
+        // Clicked event
+        "clicked", this._toggleMode.bind(this),
+
         this
       );
 
+      // Fetch DND status once
       this._sync();
     }
 
+    // Toggle DND
     _toggleMode() {
       this._settings.set_boolean(
         "show-banners",
@@ -38,6 +39,7 @@ const DndQuickToggle = GObject.registerClass(
       );
     }
 
+    // Sync DND status
     _sync() {
       const checked = !this._settings.get_boolean("show-banners");
       if (this.checked !== checked) this.set({ checked });
