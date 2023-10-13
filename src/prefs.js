b7b3fac09d41a4f6b771b0617a5c16647a5735c6
Qwreey <dev@qwreey.kr>
2023-10-14 08:14:24 +0900

    Add search option and default size

--- src/prefs.js
+++ src/prefs.js
@@ -17,6 +17,9 @@ export default class QstExtensionPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
         let settings = this.getSettings()
         
+        window.set_search_enabled(true)
+        window.set_default_size(500, 800)
+
         for (const page of pageList) {
             window.add(new page(settings, this.metadata))
         }
